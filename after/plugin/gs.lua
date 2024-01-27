37a62eae5b828d3f0b0a884a77e1c578a0544cec
Naeem Hussain <naeemnh@outlook.com>
2024-01-27 16:34:18 +0400

    fix/format fix - gs

--- after/plugin/gs.lua
+++ after/plugin/gs.lua
@@ -14,8 +14,8 @@ gs.setup({
     local gs = package.loaded.gitsigns
     local function map(mode, l, r, opts)
       opts = opts or {}
-        opts.buffer = bufnr
-        vim.keymap.set(mode, l, r, opts)
+      opts.buffer = bufnr
+      vim.keymap.set(mode, l, r, opts)
     end
 
     -- Navigation
