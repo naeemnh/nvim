0be9b4209c7de65548803051571c0a444b1236a1
Naeem Hussain <naeemnh@outlook.com>
2024-04-16 11:30:56 +0400

    add: tab and s-tab to navigate cmp options

--- lua/personal/plugins/nvim-cmp.lua
+++ lua/personal/plugins/nvim-cmp.lua
@@ -42,6 +42,15 @@ return {
 				["<C-Space>"] = cmp.mapping.complete(), -- show completion suggestions
 				["<C-e>"] = cmp.mapping.abort(), -- close completion window
 				["<CR>"] = cmp.mapping.confirm({ select = false }),
+				["<Tab>"] = cmp.mapping(function(fallback)
+					if cmp.visible() then
+						cmp.select_next_item()
+					elseif luasnip.expand_or_jumpable() then
+						luasnip.expand_or_jump()
+					else
+						fallback()
+					end
+				end, { "i", "s" }),
 			}),
 			window = {
 				documentation = {
@@ -63,6 +72,15 @@ return {
 					ellipsis_char = "...",
 				}),
 			},
+			["S-Tab"] = cmp.mapping(function(fallback)
+				if cmp.visible() then
+					cmp.select_prev_item()
+				elseif luasnip.jumpable(-1) then
+					luasnip.jump(-1)
+				else
+					fallback()
+				end
+			end, { "i", "s" }),
 		})
 	end,
 }
