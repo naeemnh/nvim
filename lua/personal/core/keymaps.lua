44308dd6f6b4ddd91f99b163dbf1d6631ceea627
Naeem Hussain <naeemnh@outlook.com>
2024-12-29 20:41:46 +0400

    fix: comments in keymaps.lua

--- lua/personal/core/keymaps.lua
+++ lua/personal/core/keymaps.lua
@@ -1,6 +1,6 @@
 vim.g.mapleader = " "
 
-local map = vim.keymap.set -- for conciseness
+local map = vim.keymap.set -- For conciseness
 
 map("i", "jk", "<ESC>", { desc = "Exit insert mode with jk" })
 map("n", "<leader><leader>", "<cmd>w<CR>", { desc = "Save file" })
@@ -10,25 +10,25 @@ map("n", "<leader>q", "<cmd>q<CR>")
 map("n", "<leader>nh", ":nohl<CR>", { desc = "Clear search highlights" })
 
 -- Increment/Decrement Numbers
-map("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- increment
-map("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- decrement
+map("n", "<leader>+", "<C-a>", { desc = "Increment number" }) -- Increment
+map("n", "<leader>-", "<C-x>", { desc = "Decrement number" }) -- Decrement
 
 -- Window Management
-map("n", "sv", "<C-w>v", { desc = "Split window vertically" }) -- split window vertically
-map("n", "ss", "<C-w>s", { desc = "Split window horizontally" }) -- split window horizontally
-map("n", "se", "<C-w>=", { desc = "Make splits equal size" }) -- make split windows equal width & height
-map("n", "sx", "<cmd>close<CR>", { desc = "Close current split" }) -- close current split window
+map("n", "sv", "<C-w>v", { desc = "Split window vertically" }) -- Split window vertically
+map("n", "ss", "<C-w>s", { desc = "Split window horizontally" }) -- Split window horizontally
+map("n", "se", "<C-w>=", { desc = "Make splits equal size" }) -- Make split windows equal width & height
+map("n", "sx", "<cmd>close<CR>", { desc = "Close current split" }) -- Close current split window
 map("n", "sh", "<C-w>h")
 map("n", "sj", "<C-w>j")
 map("n", "sk", "<C-w>k")
 map("n", "sl", "<C-w>l")
 
 -- Tab Management
-map("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- open new tab
-map("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- close current tab
-map("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  go to next tab
-map("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  go to previous tab
-map("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  move current buffer to new tab
+map("n", "<leader>to", "<cmd>tabnew<CR>", { desc = "Open new tab" }) -- Open new tab
+map("n", "<leader>tx", "<cmd>tabclose<CR>", { desc = "Close current tab" }) -- Close current tab
+map("n", "<leader>tn", "<cmd>tabn<CR>", { desc = "Go to next tab" }) --  Go to next tab
+map("n", "<leader>tp", "<cmd>tabp<CR>", { desc = "Go to previous tab" }) --  Go to previous tab
+map("n", "<leader>tf", "<cmd>tabnew %<CR>", { desc = "Open current buffer in new tab" }) --  Move current buffer to new tab
 
 -- Moving single line (Does not work om MacOS)
 map("n", "<A-w>", "<cmd>m -2<CR>", { desc = "Move line up" })
