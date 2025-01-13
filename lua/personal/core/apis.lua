fc6efd0b2953a1a391f2091b6a6447aa39d7fc07
Naeem Hussain <naeemnh@outlook.com>
2025-01-14 00:00:16 +0400

    fix: comment formatting in apis.lua

--- lua/personal/core/apis.lua
+++ lua/personal/core/apis.lua
@@ -1,8 +1,8 @@
 local api = vim.api
 local M = {}
 
--- function to create a list of commands and convert them to autocommands
--------- This fucntion is taken from https://github.com/norcalli/nvim_utils
+-- Function to create a list of commands and convert them to auto commands
+-------- This function is taken from https://github.com/norcalli/nvim_utils
 function M.nvim_create_augroups(definitions)
 	for group_name, definition in pairs(definitions) do
 		api.nvim_command("augroup " .. group_name)
@@ -16,7 +16,7 @@ function M.nvim_create_augroups(definitions)
 end
 
 local autoCommands = {
-	-- other autocommands
+	-- Other auto commands
 	open_folds = {
 		{ "BufReadPost,FileReadPost", "*", "normal zR" },
 	},
